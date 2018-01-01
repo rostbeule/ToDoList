f19d1b9e644e80ee471b798a3865b5392bccd2d2
Thomas Schröter <schroeter.th@gmail.com>
2018-01-01 16:12:43 +0100

    Initial commit.

---
+++ ToDoList/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace ToDoList
+{
+    public partial class Form1 : Form
+    {
+        private XML_Handler XML = new XML_Handler();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //TransparencyKey = BackColor;
+        }
+        
+        // Erzeuge neue XML Datei
+        private void BtnCreate_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists("ToDoList.xml"))
+            {
+                XML.Create();
+
+                MessageBox.Show("neues File wurde erzeugt");
+            }
+            else
+                MessageBox.Show("File bereits vorhanden");
+        }        
+
+        // Neuer Kalendereintrag
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            XML.AddEntry(DtpCalendar.Value.ToShortDateString(), TxtCalendar.Text);
+        }
+
+        // Lese Einträge aus XML Datei
+        private void BtnRead_Click(object sender, EventArgs e)
+        {
+            // aktuell unsortiert...
+            LblOut.Text = "unsortiert...\n\n";
+
+            XML.Read();
+
+            foreach(XML_Handler Day in XML.calendar)
+            {
+                LblOut.Text += Day.Date + Environment.NewLine;
+
+                foreach(string entry in Day.Entries)
+                {
+                    LblOut.Text += entry + Environment.NewLine;
+                }
+
+                LblOut.Text += Environment.NewLine;
+            }           
+        }
+
+        // todo...
+        private void BtnRemove_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ToDoList/XML_Handler.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace ToDoList
+{
+    class XML_Handler
+    {
+        public List<XML_Handler> calendar = new List<XML_Handler>();
+
+        private string date;
+        private string[] entries;
+
+        public XML_Handler()
+        {
+
+        }
+
+        private XML_Handler(string date, string[] entries)
+        {
+            this.date = date;
+            this.entries = entries;
+        }
+
+        public string Date
+        {
+            get { return date; }
+
+            set
+            {
+                if (string.IsNullOrEmpty(date))
+                    date = value;
+            }
+        }
+
+        public string[] Entries
+        {
+            get { return entries; }
+
+            set
+            {
+                if (entries == null)
+                    entries = value;
+            }
+        }
+
+        public void Create()
+        {
+            XmlTextWriter newFile = new XmlTextWriter("todolist.xml", new UnicodeEncoding());
+
+            newFile.WriteStartDocument();
+            newFile.WriteStartElement("Calendar");
+            newFile.WriteEndElement();
+            newFile.Close();
+        }
+
+        public void AddEntry(string date, string entry)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("todolist.xml");
+
+            // Wurzelknoten abfragen -> nur einer pro XML erlaubt
+            XmlNode root = doc.DocumentElement;
+
+            // Prüfe ob Knoten (Datum) bereits vorhanden
+            XmlNode node = doc.SelectSingleNode("//day" + date);
+
+            // wenn Knoten bereits vorhanden
+            if (node != null)
+            {
+                MessageBox.Show(date + " bereits vorhanden");
+
+                // Unterknoten (Eintrag) erzeugen
+                XmlElement newEntry = doc.CreateElement("entry");
+                newEntry.SetAttribute("note", entry);
+
+                // Knoten anhängen
+                node.AppendChild(newEntry);
+            }
+
+            // wenn Knoten noch nicht vorhanden
+            else
+            {
+                MessageBox.Show(date + " noch nicht vorhanden");
+
+                // Neuen Knoten (Datum) erzeugen
+                XmlElement newNode = doc.CreateElement("day" + date);
+
+                // Unterknoten (Eintrag) erzeugen
+                XmlElement newEntry = doc.CreateElement("entry");
+                newEntry.SetAttribute("note", entry);
+
+                // Knoten anhängen
+                newNode.AppendChild(newEntry);
+                root.AppendChild(newNode);
+            }
+
+            doc.Save("todolist.xml");
+        }
+
+        public void Read()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("todolist.xml");
+
+            XmlNode root = doc.DocumentElement;
+            GetNodes(root);
+        }
+
+        private void GetNodes(XmlNode node)
+        {
+            switch (node.NodeType)
+            {
+                case XmlNodeType.Element:
+
+                    // Erfasse Tages-Ereignisse in Array und übergebe an Obj
+                    if (node.HasChildNodes && node.Name != "Calendar")
+                    {
+                        int index = 0;
+                        string date = node.Name.Substring(3);
+                        string[] entries = new string[node.ChildNodes.Count];
+                        
+                        // Ereignisse
+                        foreach (XmlNode child in node.ChildNodes)                        
+                            foreach (XmlAttribute attr in child.Attributes)
+                            {
+                                entries[index] = attr.InnerText;
+                                index++;
+                            }
+
+                        // Füge Objekt (Tag) Liste hinzu
+                        XML_Handler Day = new XML_Handler(date, entries);                        
+                        calendar.Add(Day);
+                    }
+
+                    // rekursiver Methodenaufruf, wenn Unterknoten keine Attribute besitzt
+                    if (node.HasChildNodes)
+                        foreach (XmlNode child in node.ChildNodes)
+                            if (child.Attributes.Count == 0)
+                                GetNodes(child);
+
+                    break;
+
+                case XmlNodeType.Text:
+                    MessageBox.Show(node.Value);
+                    break;
+            }
+        }        
+    }
+}
